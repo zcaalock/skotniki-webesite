c35ad8ea87120857f844bafc3c3f4607b809ee09
zcaalock <zcaalock@yahoo.com>
2019-08-25 20:00:08 +0200

    renders links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,15 +7,15 @@ class Home extends React.Component {
   render() {
     const images = [
       {
-        original: 'img/v18a (Duży).jpg'
+        original: 'http://przyspacerowej.pl/wp-content/uploads/2019/08/v18a.jpg'
         //thumbnail: 'http://lorempixel.com/250/150/nature/1/',
       },
       {
-        original: 'img/v19a (Duży).jpg'
+        original: 'http://przyspacerowej.pl/wp-content/uploads/2019/08/v19a.jpg'
         //thumbnail: 'http://lorempixel.com/250/150/nature/2/'
       },
       {
-        original: 'img/a03b1 (Duży).jpg',
+        original: 'http://przyspacerowej.pl/wp-content/uploads/2019/08/a03b1.jpg',
         //thumbnail: 'http://lorempixel.com/250/150/nature/3/'
       }
     ]
